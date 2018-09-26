2d37aec18cad08107fe8401e51c8220c38444003
Yinuo Li <yil201@pitt.edu>
2018-09-26 17:36:26 -0400

    1000.java uploaded

---
+++ src/poj1000.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class poj1000 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt() + scan.nextInt());
+    }
+}
